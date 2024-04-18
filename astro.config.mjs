e533ff716ec855c0ecb5eaeff7abb749f9f3598a
elsergio <elsergio@gmail.com>
2024-04-17 22:45:18 -0400

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://elsergiovera.github.io/web",
-  base: 'web',
   integrations: [
     tailwind(),
     sitemap({
