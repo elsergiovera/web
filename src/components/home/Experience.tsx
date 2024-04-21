e183b0d8c9aa58b7869f9eb49dd36b9c0cfbfd11
Devas <devas@protofy.xyz>
2024-04-21 13:30:38 -0400

    fixed expand button style

--- src/components/home/Experience.tsx
+++ src/components/home/Experience.tsx
@@ -16,8 +16,8 @@ const Experience = (props) => {
             <div id="experience" ref={expRef} className={"close overflow-hidden flex items-start justify-between flex-col sm:flex-row dark:text-light"}>
                 <h2 className="w-[15rem] mt-12">EXPERIENCE</h2>
                 <div className=" w-full mt-12">
-                    {experience?.length && experience.map((xp) => (
-                        <div className="mb-12">
+                    {experience?.length && experience.map((xp, i) => (
+                        <div key={`experience-holder-${i}`} className={i === experience.length - 1 ? "mb-2" : "mb-12"}>
                             <div className="flex justify-between items-center flex-wrap">
                                 <span className="font-semibold text-xl">{xp.name}</span>
 
@@ -35,19 +35,17 @@ const Experience = (props) => {
                 </div>
             </div>
 
-            <div className='exp-separator' >
-                <button
-                    onClick={() => handleOpen()}
-                    id="readmore-button"
-                    className="readmore-button opacity-50 hover:opacity-100"
-                    aria-label="See more"
-                >
-                    {more
-                        ? <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
-                        : <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>
-                    }
-                </button>
-            </div>
+            <button
+                onClick={() => handleOpen()}
+                id="readmore-button"
+                className="readmore-button opacity-50 hover:opacity-100 hover:pt-1 dark:opacity-15 dark:hover:opacity-40"
+                aria-label="See more"
+            >
+                {more
+                    ? <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="18 15 12 9 6 15"></polyline></svg>
+                    : <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                }
+            </button>
         </>
     )
 }
