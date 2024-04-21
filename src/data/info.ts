ac0eda8c06a8b97881e6460bf83f39468d1e064a
elsergio <elsergio@gmail.com>
2024-04-21 19:28:32 -0400

    temporal fix to navbar

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,25 +1,11 @@
 export const info = {
   name: "Sergio Vera",
   about: `A Computer Science Engineer degree with more than a decade of experience as a Full Stack Developer from Santiago, Chile.`,
-  socials: [
-    { name: "github",
-      link: "https://github.com/elsergiovera",
-      icon: "<svg xmlns='http://www.w3.org/2000/svg' width='14' height='14' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z'></path><polyline points='22,6 12,13 2,6'></polyline></svg>"
-    },
-    { name: "linkedin",
-      link: "https://www.linkedin.com/in/elsergiovera",
-      icon: `<path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle>`
-    },
-    { name: "email",
-      link: "mailto:verasergio@gmail.com",
-      icon: `<path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline>`
-    }
-  ],
-  // socialMedia: {
-  //   github: "https://github.com/elsergiovera",
-  //   linkedin: "https://www.linkedin.com/in/elsergiovera/",
-  //   email: "mailto:verasergio@gmail.com"
-  // },
+  socials: {
+    github: "https://github.com/elsergiovera",
+    linkedin: "https://www.linkedin.com/in/elsergiovera/",
+    email: "mailto:verasergio@gmail.com"
+  },
   experience: [
     {
       name: "Vibia",
