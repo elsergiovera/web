d917b77f234f7931664e6319b33a12ebcd8a7886
elsergio <elsergio@gmail.com>
2024-04-22 22:54:49 -0400

    added cl emoji

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
   site: "https://veraserg.io",
   integrations: [tailwind(), mdx(), sitemap(), robotsTxt(), tailwind(), react()]
 });
