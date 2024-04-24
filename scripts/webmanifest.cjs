d9c059830476e12fc4235b6e715b7589a8501f6a
Devas <devas@protofy.xyz>
2024-04-24 12:15:46 -0400

    [test] improved webmanifest script and deploy yml to catch errors

--- scripts/webmanifest.cjs
+++ scripts/webmanifest.cjs
@@ -1,28 +1,42 @@
 const fs = require('fs');
 const info = require('../src/data/info.json');
+const manifest_path = "public/site.webmanifest"
+const { name, site_title, about } = info;
 
-const manifestData = {
-  name: info.name || "",
-  short_name: info.site_title || "",
-  description: info.about || "",
-  icons: [
-    {
+if (info) {  
+  const manifest = {
+    name: info.name || "",
+    short_name: info.site_title || "",
+    description: info.about || "",
+    icons: [
+      {
         "src": "/android-chrome-192x192.png",
         "sizes": "192x192",
         "type": "image/png"
-    },
-    {
-        "src": "/android-chrome-512x512.png",
+      },
+      {
+        "src": "/android-chrome-512x512.png", 
         "sizes": "512x512",
         "type": "image/png"
-    }
-  ],
-  theme_color: "#111111",
-  background_color: "#111111",
-  display: "standalone"
-};
+      }
+    ],
+    theme_color: "#111111",
+    background_color: "#111111",
+    display: "standalone"
+  };
 
-const manifestContent = JSON.stringify(manifestData, null, 2);
-fs.writeFileSync('public/site.webmanifest', manifestContent);
+  if (fs.existsSync(manifest_path))
+    console.info("Manifest already exists. Updating...\n")
+  
+  if (name === "" || site_title === "" || about === "")
+    console.warn("Not all details were found. Manifest may be incomplete:\n", manifest)
 
-console.log('site.webmanifest generated successfully.');
+  const manifest_content = JSON.stringify(manifest, null, 2);
+  fs.writeFileSync(manifest_path, manifest_content);
+  
+  console.log('The file site.webmanifest generated successfully.');
+  process.exit(0)
+} else {
+  console.error("No info.json was found.")
+  process.exit(1)
+}
