312ef5cc386eada6e2cd41824986f8779588c618
Devas <devas@protofy.xyz>
2024-04-26 15:54:30 -0400

    add CV button to Experience

--- src/components/home/Experience.tsx
+++ src/components/home/Experience.tsx
@@ -15,12 +15,31 @@ const Experience = (props) => {
 
     return (
         <>
-            {/* <div id="experience" ref={expRef} className={"h-96 overflow-hidden flex-col sm:flex-row dark:text-light"}> */}
-            <div id="experience" className={"flex-col sm:flex-row dark:text-light"}>
-                <h2 className="w-[15rem] mt-12">EXPERIENCE</h2>
+            <div id="experience" className={"flex-col sm:flex-row dark:text-light mt-12"}>
+                <div className={"flex items-center justify-between text-xs"}>
+                    <h2>EXPERIENCE</h2>
+                        <a
+                            href="/cv/Sergio%20Vera%20English%20CV.pdf"
+                            id="cv-link"
+                            className="not-a text-button flex"
+                            download={true}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                <polyline points="14 2 14 8 20 8"></polyline>
+                                <line x1="16" y1="13" x2="8" y2="13"></line>
+                                <line x1="16" y1="17" x2="8" y2="17"></line>
+                                <polyline points="10 9 9 9 8 9"></polyline>
+                            </svg>
+                            <hr
+                                className="text-button-separator"
+                            />
+                            &nbsp;Download CV
+                        </a>
+                </div>
                 <div className=" w-full mt-12">
                     {experience?.length && experience.map((xp, i) => (
-                        <div key={`experience-holder-${i}`} className={"container text-sm mb-10"}>
+                        <div key={`experience-holder-${i}`} className={"text-sm mb-10 md:px-2"}>
                             <div className="flex justify-between items-center flex-wrap">
                                 <span className="font-semibold text-xl">{xp.name}</span>
 
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,21 +3,23 @@ module.exports = {
 	darkMode: ["class", '[data-theme="dark"]'],
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-	  extend: {
-		colors: {
-		  dark: "#111111",
-		  light: "#FFFFFF",
-		  blue: "#33d2ff",
-		  gray: "#C4C4C4",
+		extend: {
+			screens: {
+				'xs': '500px'
+			},
+			colors: {
+				dark: "#111111",
+				light: "#FFFFFF",
+				blue: "#33d2ff",
+				gray: "#C4C4C4",
+			},
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				outfit: ["Outfit", "sans-serif"],
+			},
 		},
-		fontFamily: {
-		  inter: ["Inter", "sans-serif"],
-		  outfit: ["Outfit", "sans-serif"],
-		},
-	  },
 	},
 	plugins: [
 		require('@tailwindcss/typography')
 	],
-  };
-  
+};
